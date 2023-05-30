5cc8f1bbbeecb0d1036415d52098866176da5995
user <user@example.org>
2023-05-30 20:31:28 +0200

    add backlight temperature

--- arch-graphical-install-auto.sh
+++ arch-graphical-install-auto.sh
@@ -649,11 +649,11 @@ aurinstaller qt5-styleplugins
 aurinstaller passdmenu
 
 # background temparature
-#aurinstaller wl-gammarelay-rs
-#if [ "${archisoinstall}" == "y" ]; then
-#    echo "Entferne wl-gammerelay-rs Ordner um Speicherplatz zu sparen"
-#    rm -R /home/${user}/aur-builds/wl-gammarelay-rs
-#fi
+aurinstaller wl-gammarelay-rs
+if [ "${archisoinstall}" == "y" ]; then
+    echo "Entferne wl-gammerelay-rs Ordner um Speicherplatz zu sparen"
+    rm -R /home/${user}/aur-builds/wl-gammarelay-rs
+fi
 
 # thinkpad docking station Ultra
 #aurinstaller evdi-git
