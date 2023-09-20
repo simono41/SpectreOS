77724c1ece309ef8184a535c06ba7ff59f0364ac
Simon Rieger <simono41@brothertec.eu>
2023-09-20 10:05:32 +0200

    add udev rules

--- arch-graphical-install-auto.sh
+++ arch-graphical-install-auto.sh
@@ -665,6 +665,11 @@ aurinstaller dotool
 aurinstaller python-vosk-bin
 aurinstaller nerd-dictation-git
 
+echo '## Give ydotoold access to the uinput device
+## Solution by https://github.com/ReimuNotMoe/ydotool/issues/25#issuecomment-535842993
+KERNEL=="uinput", GROUP="input", MODE="0660", OPTIONS+="static_node=uinput"
+' > /etc/udev/rules.d/80-uinput.rules
+
 # thinkpad docking station Ultra
 #aurinstaller evdi-git
 #aurinstaller displaylink
